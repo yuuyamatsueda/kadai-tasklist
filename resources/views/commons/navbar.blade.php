2af4f701dd08ba6a643fb2690eb87146860c160a
yuuya.matsueda <matsuedayuya0407@gmail.com>
2020-08-10 12:58:52 +0000

    login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -7,12 +7,48 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="nav-bar">
+<div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- タスク作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                @if (Auth::check())
+                    {{-- ユーザ一覧ページへのリンク --}}
+                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            {{-- ユーザ詳細ページへのリンク --}}
+                            <li class="dropdown-item"><a href="#">My profile</a></li>
+                            <li class="dropdown-divider"></li>
+                            {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
+
+        <!--<div class="collapse navbar-collapse" id="nav-bar">-->
+        <!--    <ul class="navbar-nav mr-auto"></ul>-->
+        <!--  <ul class="navbar-nav">-->
+        <!--    <ul class="dropdown-menu dropdown-menu-right"> -->
+        <!--       @if (Auth::check())-->
+        <!--        {{-- タスク作成ページへのリンク --}}-->
+        <!--        <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>-->
+        <!--        {{-- ログアウトへのリンク --}}-->
+        <!--        <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>-->
+        <!--        @else-->
+        <!--            {{-- ユーザ登録ページへのリンク --}}-->
+        <!--            <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>-->
+        <!--            {{-- ログインページへのリンク --}}-->
+        <!--            <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>-->
+        <!--        @endif-->
+        <!--    </ul>    -->
+        <!--    </ul>-->
+        <!--</div>-->
     </nav>
 </header>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
 
+@if (Auth::check())
+<!--// ログイン済み-->
 <h1>タスク一覧</h1>
 
-    @if (count($tasks) > 0)
+@if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -26,7 +28,17 @@
         </table>
     @endif
 
-{{-- タスク作成ページへのリンク --}}
+    {{-- タスク作成ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
+@else
+<!--// ログインまだ-->
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the TaskList</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
+@endif
 
 @endsection
