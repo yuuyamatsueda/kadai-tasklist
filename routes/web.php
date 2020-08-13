60f0c5aa61b49a6d878ee1cddc3ac78b07d68fb3
yuuya.matsueda <matsuedayuya0407@gmail.com>
2020-08-13 09:22:51 +0000

    syusei 5

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,6 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 Route::group(['middleware' => ['auth']], function () {
-     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
-    Route::resource('tasks', 'TasksController', ['only' => ['store', 'destroy']]);
+     
+    Route::resource('tasks', 'TasksController', ['only' => ['store', 'destroy','create','show','edit','update']]);
 });
